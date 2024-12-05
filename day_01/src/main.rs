cebf08369d8cdfff77348adbb971c7075f515f4e
Stefan Bobev <stefanbobev@hotmail.com>
2024-12-05 20:58:54 +0000

    Solve the second part of the first problem.
    This was also quite easy. There is a good example of some of the nice
    APIs that HashMaps have in Rust.

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use std::{env, path};
+use std::{collections::HashMap, env, path};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -14,6 +14,8 @@ fn main() {
     let cli_inputpath = path::Path::new(&args.input);
 
     let input_data = aocutils::read_input_file_by_line(cli_inputpath, crate_name);
+
+    // #region First part of the problem
 
     let (first_column, second_column) = input_data
         .iter()
@@ -35,10 +37,31 @@ fn main() {
     c2.sort_unstable();
 
     let total_distance = c1
-        .into_iter()
-        .zip(c2)
+        .iter()
+        .zip(&c2)
         .map(|(n1, n2)| i32::abs(n1 - n2))
         .sum::<i32>();
 
     println!("Total distance is: {}", total_distance);
+
+    // #endregion
+
+    // #region Second part of the problem
+
+    let mut occurrences = HashMap::<i32, i32>::new();
+
+    c2.into_iter()
+        .for_each(|number| *occurrences.entry(number).or_insert(0) += 1);
+
+    let similarity_score = c1.iter().fold(0, |acc, number| {
+        // TODO: (Stefan) I am not sure exactly what `or_insert` and `or_default` differ in. I am hoping,
+        // that I am only probing the map and not trying to insert into it (unlike the one above). Also
+        // don't like that "Default" is implicit from the trait `Default` for i32 - what if I want a weird
+        // default?
+        acc + number * (*occurrences.entry(*number).or_default())
+    });
+
+    println!("The similarity score is: {}", similarity_score);
+
+    // #endregion
 }
