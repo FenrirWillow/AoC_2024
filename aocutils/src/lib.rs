4d4b6e42bc29de42c41b110c3826f78de200cb34
Stefan Bobev <stefanbobev@hotmail.com>
2024-12-05 12:56:54 +0000

    Add a simple solution to day 01.
    The first day is now complete. Pretty straight forward solution, albeit
    with some struggles when it comes to tuples and iterators.

--- aocutils/src/lib.rs
+++ aocutils/src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::{
+    env, fs,
+    io::{self, BufRead},
+    path,
+};
+
+// TODO: (Stefan) Figure out how logging should work!
+
+/// TODO: (Stefan) Document me!
+pub fn adjust_input_filepath(
+    cli_provided_path: &path::Path,
+    executing_crate: &str,
+) -> path::PathBuf {
+    if let Ok(root) = env::var("BUILD_WORKING_DIRECTORY") {
+        let full_path = path::Path::new(&root)
+            .join(executing_crate)
+            .join("inputs")
+            .join(cli_provided_path);
+
+        return full_path;
+    }
+
+    cli_provided_path.to_path_buf()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// TODO: (Stefan) Document me!
+pub fn read_input_file_whole(cli_provided_path: &path::Path, executing_crate: &str) -> String {
+    let input_filepath = adjust_input_filepath(cli_provided_path, executing_crate);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    fs::read_to_string(input_filepath).unwrap()
+}
+
+/// TODO: (Stefan) Document me!
+pub fn read_input_file_by_line(
+    cli_provided_path: &path::Path,
+    executing_crate: &str,
+) -> Vec<String> {
+    let input_filepath = adjust_input_filepath(cli_provided_path, executing_crate);
+
+    let file = fs::File::open(input_filepath).unwrap();
+
+    io::BufReader::new(file)
+        .lines()
+        .map_while(Result::ok)
+        .collect::<Vec<String>>()
 }
--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,21 +1,44 @@
 use clap::Parser;
+use std::{env, path};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
-
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    #[arg(short, long, default_value = "example.txt")]
+    input: String,
 }
 
 fn main() {
     let args = Args::parse();
+    let crate_name = clap::crate_name!();
+    let cli_inputpath = path::Path::new(&args.input);
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name);
-    }
+    let input_data = aocutils::read_input_file_by_line(cli_inputpath, &crate_name);
+
+    let (first_column, second_column) = input_data
+        .iter()
+        .flat_map(|line| line.split_whitespace().map(|c| c.parse::<i32>().unwrap()))
+        .enumerate()
+        .partition::<Vec<_>, _>(|&(i, _)| i % 2 == 0);
+
+    let mut c1 = first_column
+        .into_iter()
+        .map(|(_idx, value)| value)
+        .collect::<Vec<i32>>();
+
+    let mut c2 = second_column
+        .into_iter()
+        .map(|(_idx, value)| value)
+        .collect::<Vec<i32>>();
+
+    c1.sort_unstable();
+    c2.sort_unstable();
+
+    let total_distance = c1
+        .into_iter()
+        .zip(c2)
+        .map(|(n1, n2)| i32::abs(n1 - n2))
+        .sum::<i32>();
+
+    println!("Total distance is: {}", total_distance);
 }
