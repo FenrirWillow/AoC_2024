6230dcad844331fe306ce5387585eaa20eb535f6
Stefan Bobev <stefanbobev@hotmail.com>
2024-12-06 11:08:54 +0000

    Add the second part of the secon day.
    This was really satisfying to solve. The ability to create all
    combinations of length using itertools was a godsend and allowed me to
    focus on the main logic quite quickly.
    
    There are a few refactorings I would probably do if this were production
    code, mainly try to understand why the references get wonky with
    `itertools::Combinations` (see the ugly vector transform in Part Two)
    and using a PartitionMap to generate the "safe by default" and
    "potentially safe" sets.

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,12 +1,39 @@
 use clap::Parser;
 use itertools::{FoldWhile, Itertools};
-use std::{env, path};
+use std::{collections::HashSet, env, path};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(short, long, default_value = "example.txt")]
     input: String,
+}
+
+fn is_level_safe(level_values: &[i32]) -> bool {
+    let final_difference = level_values
+        .iter()
+        .tuple_windows()
+        .fold_while::<i32, _>(0, |previous_difference, (n1, n2)| {
+            let diff = n1 - n2;
+            let abs_diff = i32::abs(diff);
+
+            if !(1..=3).contains(&abs_diff) {
+                // NOTE: (Stefan) This level is considered "unsafe", because
+                // the value of the difference is not within the specified range
+                return FoldWhile::Done(0);
+            }
+
+            if previous_difference != 0 && previous_difference.signum() != diff.signum() {
+                // NOTE: (Stefan) This level is considered "unsafe", because
+                // the difference changed "direction" from last window
+                return FoldWhile::Done(0);
+            }
+
+            FoldWhile::Continue(diff)
+        })
+        .into_inner();
+
+    final_difference != 0
 }
 
 fn main() {
@@ -16,41 +43,67 @@ fn main() {
 
     let input_data = aocutils::read_input_file_by_line(cli_inputpath, crate_name);
 
-    let safe_level_count = input_data
+    let reports = input_data
         .iter()
         .map(|line| {
             line.split_whitespace()
                 .map(|c| c.parse::<i32>().unwrap())
                 .collect::<Vec<_>>()
         })
-        .map(|level_values| {
-            let final_difference = level_values
-                .iter()
-                .tuple_windows()
-                .fold_while::<i32, _>(0, |previous_difference, (n1, n2)| {
-                    let diff = n1 - n2;
-                    let abs_diff = i32::abs(diff);
+        .collect::<Vec<_>>();
 
-                    if !(1..=3).contains(&abs_diff) {
-                        // NOTE: (Stefan) This level is considered "unsafe", because
-                        // the value of the difference is not within the specified range
-                        return FoldWhile::Done(0);
-                    }
+    let mut safe_by_default = HashSet::<usize>::new();
 
-                    if previous_difference != 0 && previous_difference.signum() != diff.signum() {
-                        // NOTE: (Stefan) This level is considered "unsafe", because
-                        // the difference changed "direction" from last window
-                        return FoldWhile::Done(0);
-                    }
+    // #region Part One
 
-                    FoldWhile::Continue(diff)
-                })
-                .into_inner();
+    let safe_level_count = reports
+        .iter()
+        .map(|level_values| is_level_safe(level_values))
+        .enumerate()
+        .map(|(idx, is_safe)| {
+            // NOTE: (Stefan) This may be cleaner with a partition map.
+            // We can separate the levels into "safe_by_default" and "potentially_safe"
+            // We can then use the "potentially_safe" levels as the source for the iterator
+            // in the second part.
+            if is_safe {
+                safe_by_default.insert(idx);
+            }
 
-            final_difference != 0
+            is_safe
         })
         .filter(|is_safe| *is_safe)
         .count();
 
-    println!("The total number of safe levels is: {}", safe_level_count)
+    println!("The total number of safe levels is: {}", safe_level_count);
+
+    // #endregion
+
+    // #region Part Two
+
+    let safe_with_dampen_count = reports
+        .iter()
+        .enumerate()
+        .filter(|(idx, _)| !safe_by_default.contains(idx))
+        .map(|(_, level_values)| {
+            level_values
+                .iter()
+                .combinations(level_values.len() - 1)
+                // NOTE: (Stefan) The following map is needed because the combinations iterator creates a really weird iterator type. The
+                // transform makes it slightly more manageable.
+                // REFACTOR: (Stefan) I wonder if there is a cleaner way to deal with this?
+                .map(|subset_level_values| {
+                    subset_level_values.iter().map(|v| **v).collect::<Vec<_>>()
+                })
+                .map(|subset_level_values| is_level_safe(&subset_level_values))
+                .any(|is_subset_safe| is_subset_safe)
+        })
+        .filter(|any_subset_safe| *any_subset_safe)
+        .count();
+
+    println!(
+        "The total number of safe levels (with dampener on) is: {}",
+        safe_level_count + safe_with_dampen_count
+    );
+
+    // #endregion
 }
