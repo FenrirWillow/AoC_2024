168188a55c72d7febffe3d04bf97552b0da7d2c0
Stefan Bobev <stefanbobev@hotmail.com>
2024-12-06 22:01:38 +0000

    Add the second part of day 03.
    Still easy, required named capture groups in order to make the API nice.
    This is a good example where I feel like there should be a pure
    functional way to solve this, but my knowledge of arcane itertools/std
    functions fails me.
    
    The part I really don't like is how we must yield an element in the
    capture group map, so that we can sum it in the end. But in the
    imperative version, we would have a very clear branch. I don't know what
    I want it to look like really, but I have a feeling this ain't it.

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -15,13 +15,60 @@ fn main() {
 
     let input_data = aocutils::read_input_file_whole(cli_inputpath, crate_name);
 
-    let re = regex::Regex::new(r"mul\((\d{1,3}),\s*(\d{1,3})\)").unwrap();
+    // #region Part One
 
-    let total_mul_sum = re
+    let only_mul_ops_re = regex::Regex::new(r"mul\((\d{1,3}),\s*(\d{1,3})\)").unwrap();
+
+    let total_mul_sum = only_mul_ops_re
         .captures_iter(&input_data)
         .map(|capture_group| capture_group.extract())
         .map(|(_, [a, b])| a.parse::<i32>().unwrap() * b.parse::<i32>().unwrap())
         .sum::<i32>();
 
-    println!("Total sum of all multiples: {total_mul_sum}",);
+    println!("Total sum of all mul ops: {total_mul_sum}",);
+
+    // #endregion
+
+    // #region Part Two
+
+    let mul_ops_and_ctrl_re =
+        regex::Regex::new(r"(?<ops>mul\((?<a>\d{1,3}),\s*(?<b>\d{1,3})\))|(?<ctrl>do(?:n't)?\(\))")
+            .unwrap();
+
+    let mut accept_new_ops = true;
+
+    let total_mul_sum_with_ctrl = mul_ops_and_ctrl_re
+        .captures_iter(&input_data)
+        .map(|capture_group| {
+            if capture_group.name("ops").is_some() {
+                if accept_new_ops {
+                    let a = capture_group
+                        .name("a")
+                        .unwrap()
+                        .as_str()
+                        .parse::<i32>()
+                        .unwrap();
+                    let b = capture_group
+                        .name("b")
+                        .unwrap()
+                        .as_str()
+                        .parse::<i32>()
+                        .unwrap();
+
+                    return a * b;
+                } else {
+                    return 0;
+                }
+            } else if let Some(control_op) = capture_group.name("ctrl") {
+                accept_new_ops = control_op.as_str() == "do()";
+                return 0;
+            }
+
+            unreachable!("The capture group was neither a mul op NOR a control op!");
+        })
+        .sum::<i32>();
+
+    println!("Total sum of all mul ops (respecing control ops): {total_mul_sum_with_ctrl}",)
+
+    // #endregion
 }
